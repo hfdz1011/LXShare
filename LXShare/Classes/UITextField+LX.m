a017fdd56ba76fa1d45d95c1a9b09d022eef6c5d
hfdz1011 <18576722857@163.com>
2020-09-18 16:43:37 +0800

    修改UITextField占位符的颜色和字体的设置

--- LXShare/Classes/UITextField+LX.m
+++ LXShare/Classes/UITextField+LX.m
@@ -17,15 +17,17 @@
     filed.font = font;
     filed.textColor = color;
     
-    if ([UIDevice currentDevice].systemVersion.floatValue >= 13.0) {
-        
-        filed.attributedPlaceholder = [[NSAttributedString alloc] initWithString:placeString attributes:@{NSForegroundColorAttributeName: placeColor,NSFontAttributeName:placeFont}];
-    }else{
-        
-        filed.placeholder = placeString;
-        [filed setValue:placeColor forKeyPath:@"_placeholderLabel.textColor"];
-        [filed setValue:NSProgressFileOperationKindDecompressingAfterDownloading forKeyPath:@"_placeholderLabel.font"];
-    }
+     filed.attributedPlaceholder = [[NSAttributedString alloc] initWithString:placeString attributes:@{NSForegroundColorAttributeName: placeColor,NSFontAttributeName:placeFont}];
+    
+//    if ([UIDevice currentDevice].systemVersion.floatValue >= 13.0) {
+//
+//        filed.attributedPlaceholder = [[NSAttributedString alloc] initWithString:placeString attributes:@{NSForegroundColorAttributeName: placeColor,NSFontAttributeName:placeFont}];
+//    }else{
+//
+//        filed.placeholder = placeString;
+//        [filed setValue:placeColor forKeyPath:@"_placeholderLabel.textColor"];
+//        [filed setValue:NSProgressFileOperationKindDecompressingAfterDownloading forKeyPath:@"_placeholderLabel.font"];
+//    }
     
     return filed;
 }
@@ -36,15 +38,17 @@
 + (UITextField *)lx_setTextFiled:(UITextField *)filed withPlace:(NSString *)place withPlaceColor:(UIColor *)color withFont:(UIFont *)font
 {
     
-    if ([UIDevice currentDevice].systemVersion.floatValue >= 13.0) {
-        
-        filed.attributedPlaceholder = [[NSAttributedString alloc] initWithString:place attributes:@{NSForegroundColorAttributeName: color,NSFontAttributeName:font}];
-    }else{
-        
-        filed.placeholder = place;
-        [filed setValue:color forKeyPath:@"_placeholderLabel.textColor"];
-        [filed setValue:font forKeyPath:@"_placeholderLabel.font"];
-    }
+     filed.attributedPlaceholder = [[NSAttributedString alloc] initWithString:place attributes:@{NSForegroundColorAttributeName: color,NSFontAttributeName:font}];
+    
+//    if ([UIDevice currentDevice].systemVersion.floatValue >= 13.0) {
+//        
+//        filed.attributedPlaceholder = [[NSAttributedString alloc] initWithString:place attributes:@{NSForegroundColorAttributeName: color,NSFontAttributeName:font}];
+//    }else{
+//        
+//        filed.placeholder = place;
+//        [filed setValue:color forKeyPath:@"_placeholderLabel.textColor"];
+//        [filed setValue:font forKeyPath:@"_placeholderLabel.font"];
+//    }
     return filed;
 }
 
